1d48a5eb802aa543951f95b5f287dc4a8b74d0f5
unknown <keith.barrow@TOMBOLA-0361.tombola.emea>
2014-11-19 17:34:54 +0000

    Declaring Variables & fundatmental types.

---
+++ CodeSamples/CodeSamples/DeclaringVariableTypes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSamples
+{
+	public static class DeclaringVariableTypes
+	{
+		public static void Test()
+		{
+			bool fooBool = true;
+			short fooShort = 1; //16 bit int
+			int fooInt = 3; //32 bit int
+			long fooLong = 1; //64 bit int
+
+			//Unsigned versions of above = positve numbers only, but can go twice as high
+			ushort fooUShort = 1;
+			uint fooUInt = 1;
+			ulong fooULong = 1;
+
+			//same as short, int, long, different declaration -there are also unsigned versions!
+			Int16 fooInt16 = 1;
+			Int32 fooInt32 = 1;
+			Int64 fooInt63 = 1;
+
+
+			//uint dooo = -1; //can't do this, negative
+
+
+			//Maths values have max and min values. Unsigned values have a higher max value
+			Console.WriteLine("Max long:" + long.MaxValue);
+			Console.WriteLine("Max ulong:" + ulong.MaxValue);
+			Console.WriteLine("Min long:" + long.MinValue);
+			Console.WriteLine("Min ulong:" + ulong.MinValue);
+		}
+	}
+}
---
+++ CodeSamples/CodeSamples/WhyNotToUseFloatingPointTypes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSamples
+{
+	public static class WhyNotToUseFloatingPointTypes
+	{
+		public static void Test()
+		{
+			//Don't use float - there are precision problems, same principle applies to "double" type as just a larger float.
+			float f1 = 1.0f / 5.0f; //One fifth in maths, near representation of one fifth as float.
+			float f2 = (f1 * 5.0f) - 1.0f;   // Maths  = (1/5 * 5) - = 0!. But our double 1/5 is an approximation...
+			Console.WriteLine(f2 == 0.0f);   // So this comparison fails.
+
+			//But the same calc works OK for decimals due to the different representation in memory.
+			decimal d1 = 1.0M / 5.0M; 
+			decimal d2 = (d1 * 5.0M) - 1.0M;   
+			Console.WriteLine(d2 == 0.0M);
+			//TOP KEK.
+		}
+	}
+}
